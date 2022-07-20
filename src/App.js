144dc991ea50acf06f80ad1481e6835f2bfb15dc
Tuan Pham Anh <baabeetaa@gmail.com>
2022-07-20 11:06:26 +0700

    test cors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{
+    const [resjson, setResjson] = useState(0);
+
+    const handleClick = async () => {
+        const status_res = await fetch("https://api-evmos-ia.notional.ventures/cosmos/gov/v1beta1/proposals?pagination.limit=50&pagination.reverse=true", {compress: false});
+        const res_json = await status_res.json();
+
+        // this.setState({res_json: res_json});
+        setResjson(res_json)
+    }
+
+    return (
+        <div className="App">
+            <br/><br/><br/><br/>
+
+            <button onClick={handleClick}>Click to test CORS</button>
+
+            <pre style={{whiteSpace: 'pre-wrap'}}>{JSON.stringify(resjson)}</pre>
+
+        </div>
+    );
 }
 
 export default App;
